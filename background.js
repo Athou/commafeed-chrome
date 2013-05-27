6e41f500d61d915247309acae65fefd2a3f6be24
Jeremie Panzer <jeremie.panzer@erasme.ulb.ac.be>
2013-05-27 12:33:54 +0200

    focus on an existing tab if it exists, open a new one otherwise

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 var interval = 60000;
 var refresh = function() {
-    var url = localStorage["url"] || "http://www.commafeed.com";
+    var url = localStorage["url"] || "https://www.commafeed.com";
 	if (url.lastIndexOf('/') != url.length - 1) {
 	  url += '/';
 	}
@@ -24,9 +24,19 @@ var refresh = function() {
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var url = localStorage["url"] || "http://www.commafeed.com";
-  chrome.tabs.create({'url': url}, function(tab) {
-    // do nothing
+  var url = localStorage["url"] || "https://www.commafeed.com";
+  if (url.lastIndexOf('/') != url.length - 1) {
+	  url += '/';
+  }
+  var pattern = url + '*';
+  chrome.tabs.query({url: pattern}, function(array) {
+	if(array.length === 0) {
+	    chrome.tabs.create({'url': url}, function(tab) {
+		  // do nothing
+		});
+	} else {
+	  chrome.tabs.update(array[0].id, {active: true});
+	}
   });
 });
 
